ba4a2a7d4b0570d931d818c5076c1e25b6c46d9f
Dikshant Gupta <dikshant2210@gmail.com>
2020-07-18 17:03:42 +0200

    results b0

--- test.py
+++ test.py
@@ -359,7 +359,7 @@ def get_net():
     net = EfficientNet.from_pretrained('efficientnet-b0')
     num_ftrs = net._fc.in_features
     net._fc = nn.Linear(in_features=num_ftrs, out_features=4, bias=True)
-    net.load_state_dict(torch.load('weights/b1/best-checkpoint-b0-008epoch.bin')['model_state_dict'])
+    net.load_state_dict(torch.load('weights/b0/best-checkpoint-b0-008epoch.bin')['model_state_dict'])
     return net
 
 
