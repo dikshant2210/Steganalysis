fdc6e989430826eaeae58f61ebf4c6abcafc7259
Dikshant Gupta <dikshant2210@gmail.com>
2020-07-19 11:03:56 +0200

    submission file

--- test.py
+++ test.py
@@ -286,7 +286,7 @@ class Fitter:
                 images = images.to(self.device).float()
                 outputs = self.model(images)
                 _, pred = torch.max(outputs, dim=1)
-                for y_p, y_t in zip(pred, targets):
+                for y_p, y_t in zip(outputs, targets):
                     y_true.append(y_t)
                     y_pred.append(y_p)
                 loss = self.criterion(outputs, targets)
@@ -416,3 +416,52 @@ def run_training():
 
 
 run_training()
+
+class DatasetSubmissionRetriever(Dataset):
+
+    def __init__(self, image_names, transforms=None):
+        super().__init__()
+        self.image_names = image_names
+        self.transforms = transforms
+
+    def __getitem__(self, index: int):
+        image_name = self.image_names[index]
+        image = cv2.imread(f'{DATA_ROOT_PATH}/Test/{image_name}', cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+        if self.transforms:
+            sample = {'image': image}
+            sample = self.transforms(**sample)
+            image = sample['image']
+
+        return image_name, image
+
+    def __len__(self) -> int:
+        return self.image_names.shape[0]
+
+dataset = DatasetSubmissionRetriever(
+    image_names=np.array([path.split('/')[-1] for path in glob('data/Test/*.jpg')]),
+    transforms=get_valid_transforms(),
+)
+
+data_loader = DataLoader(
+    dataset,
+    batch_size=8,
+    shuffle=False,
+    num_workers=2,
+    drop_last=False,
+)
+
+result = {'Id': [], 'Label': []}
+for step, (image_names, images) in enumerate(data_loader):
+    print(step, end='\r')
+
+    y_pred = net(images.cuda())
+    y_pred = 1 - nn.functional.softmax(y_pred, dim=1).data.cpu().numpy()[:, 0]
+
+    result['Id'].extend(image_names)
+    result['Label'].extend(y_pred)
+
+submission = pd.DataFrame(result)
+submission.to_csv('submission.csv', index=False)
+submission.head()
